3956646a7c3d5b243713d7510d888abe14a1c72d
kure-kg <stashakg@gmail.com>
2020-09-24 08:06:17 +0200

    Arh

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -15,19 +15,10 @@ class HomeController extends Controller
         $model_kategorije = new Kategorija();
         $kategorije = $model_kategorije->all();
 
-        $model_clanak = new Clanak();
-        $sql = "SELECT *
-                FROM   clanci
-                WHERE  objavljen = 1
-                ORDER  BY published_at DESC
-                LIMIT  1;";
-        $clanak = $model_clanak->fetch($sql);
-
         $model = new Clanak();
         $clanci = $model->paginate($this->page(), 'page', 
             "SELECT * FROM clanci
             WHERE  objavljen = 1
-            AND arhiviran = 0 
             AND deleted_at IS NULL
             ORDER BY published_at DESC;");
 
